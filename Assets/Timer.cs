a21b23a87c5bd7ad3eb5512126487c9cd2a3d870
atodd-qub <atodd11@qub.ac.uk>
2024-01-28 15:18:56 +0000

    timer UI

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,24 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
     private float timer;
     private int minutes;
-    private string seconds; 
+    private string seconds;
+
+    public TextMeshProUGUI timerUI;
 
     // Start is called before the first frame update
     void Start()
     {
         // set timer to X minutes
-
+        timer = 90;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateTimer();
     }
 
     private void UpdateTimer()
@@ -26,16 +30,18 @@ public class Timer : MonoBehaviour
         // update race time
         timer -= Time.deltaTime;
         minutes = ((int)timer / 60);
-        seconds = (timer % 60).ToString("F3");
+        seconds = (timer % 60).ToString("F0");
 
         // use minutes/seconds to display time in UI
         if (minutes == 0)
         {
             //timerUI.text = seconds;
+            timerUI.SetText($"{seconds}");
         }
         else
         {
             //timerUI.text = (minutes + ":" + seconds).ToString();
+            timerUI.SetText($"{minutes}:{seconds}");
         }
 
 
