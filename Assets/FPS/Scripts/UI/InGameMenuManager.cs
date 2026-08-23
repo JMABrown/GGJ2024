50f69939d5c6d8af4135eda6dc4cdd80a30641bd
jmabrown <james.mich.alex.brown@gmail.com>
2026-08-23 13:11:42 +0100

    Add remember details checkbox that does nothing

--- Assets/FPS/Scripts/UI/InGameMenuManager.cs
+++ Assets/FPS/Scripts/UI/InGameMenuManager.cs
@@ -76,8 +76,11 @@ public class InGameMenuManager : MonoBehaviour
         ShadowsToggle.isOn = QualitySettings.shadows != ShadowQuality.Disable;
         ShadowsToggle.onValueChanged.AddListener(OnShadowsChanged);
 
-        InvincibilityToggle.isOn = m_PlayerHealth.Invincible;
-        InvincibilityToggle.onValueChanged.AddListener(OnInvincibilityChanged);
+        if (InvincibilityToggle)
+        {
+            InvincibilityToggle.isOn = m_PlayerHealth.Invincible;
+            InvincibilityToggle.onValueChanged.AddListener(OnInvincibilityChanged);
+        }
 
         FramerateToggle.isOn = m_FramerateCounter.UIText.gameObject.activeSelf;
         FramerateToggle.onValueChanged.AddListener(OnFramerateCounterChanged);
